13075972b4dda2e14bd6f7ab0b7e79b9d5e88faf
Thahira Thasneem <thahira767@gmail.com>
2024-11-14 19:02:25 +0400

    Update Hard Level.sql

--- MySQL/Hard Level.sql
+++ MySQL/Hard Level.sql
@@ -8,6 +8,21 @@ WITH new_cte AS(
   SELECT date FROM new_cte
 WHERE temperature > temp2;
 
+-- Cake Vs Pie
+-- Solution:
+
+SELECT date_sold, 
+ABS(SUM(CASE WHEN product = 'Cake' THEN amount_sold ELSE 0 END) -
+    SUM(CASE WHEN product = 'Pie' THEN amount_sold ELSE 0 END)) AS difference, 
+(CASE WHEN
+SUM(CASE WHEN product = 'Cake' THEN amount_sold ELSE 0 END) >
+SUM(CASE WHEN product = 'Pie' THEN amount_sold ELSE 0 END)
+THEN 'Cake' ELSE 'Pie' END) AS sold_more 
+FROM desserts
+GROUP BY date_sold
+ORDER BY date_sold
+;
+
 -- Kelly's 3rd Purchase
 -- Solution:
 
